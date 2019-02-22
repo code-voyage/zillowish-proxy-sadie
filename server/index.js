2d5168da372de2fb644a1444cefae907f983abd7
Sadie Gill <thesadie@gmail.com>
2019-02-22 08:44:00 -0800

    Adjusts base url to give access to id param in url

--- server/index.js
+++ server/index.js
@@ -4,16 +4,8 @@ const axios = require('axios')
 
 const port = 3000;
 
-app.use(express.static('public'));
+app.use('/:id', express.static('public'));
 
 app.listen(port, () => {
   console.log(`listening on port: ${port}`)
-})
-
-app.get('/:id', (req, res) => {
-  axios.get(`http://localhost:3001/houses/${req.params.id}`)
-  .then(response => {
-    res.send(response.data)
-  })
-  .catch(err => {console.log(err)})
 })
