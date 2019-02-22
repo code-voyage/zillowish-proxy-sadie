609570d2e3ea73805a8a2a7756de5190c218e7ed
Sadie Gill <thesadie@gmail.com>
2019-02-22 07:43:34 -0800

    creates route for url with parm id

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const axios = require('axios')
 
 const port = 3000;
 
@@ -7,4 +8,12 @@ app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`listening on port: ${port}`)
+})
+
+app.get('/:id', (req, res) => {
+  axios.get(`http://localhost:3001/houses/${req.params.id}`)
+  .then(response => {
+    res.send(response.data)
+  })
+  .catch(err => {console.log(err)})
 })
